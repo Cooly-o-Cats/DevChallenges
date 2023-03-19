3f3fec919ce9ba20ae6a805e4fdf0bc45ee67572
Liamr <liamreitsma@icloud.com>
2023-03-19 13:01:52 -0500

    changing deploy

--- recipe-page/src/components/Header.js
+++ recipe-page/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import Recipe from "https://cooly-o-cats.github.io/DevChallenges/recipe-page/public/recipe.json"
+console.log(Recipe)
 const Header = () => {
   return (
     <div>Header</div>
